6dde41576dad307949791208eb90f4413c3d7ff5
MATHIEU STROSBERG <amthieu@mathieu-strosberg.home>
2021-06-20 19:52:18 +0200

    update img

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -3,8 +3,15 @@ import withAutoplay from "react-awesome-slider/dist/autoplay";
 import "react-awesome-slider/dist/styles.css";
 
 import img1 from "../assets/images/1.jpg";
-import img2 from "../assets/images/2.JPEG";
+import img2 from "../assets/images/2.jpg";
 import img3 from "../assets/images/3.JPEG";
+import img4 from "../assets/images/4.jpg";
+import img5 from "../assets/images/5.jpg";
+import img6 from "../assets/images/6.jpg";
+import img7 from "../assets/images/7.jpg";
+import img8 from "../assets/images/8.jpg";
+import img9 from "../assets/images/9.jpg";
+import img10 from "../assets/images/10.jpg";
 
 const Slider = () => {
   const AutoplaySlider = withAutoplay(AwesomeSlider);
@@ -20,6 +27,13 @@ const Slider = () => {
         <div data-src={img1} />
         <div data-src={img2} />
         <div data-src={img3} />
+        <div data-src={img4} />
+        <div data-src={img5} />
+        <div data-src={img6} />
+        <div data-src={img7} />
+        <div data-src={img8} />
+        <div data-src={img9} />
+        <div data-src={img10} />
       </AutoplaySlider>
     </div>
   );
